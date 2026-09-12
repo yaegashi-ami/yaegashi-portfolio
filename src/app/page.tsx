1213ffa28436957569cc75869fb44d3dfc6ffe93
arcward-con <68462635+yaegashi-ami@users.noreply.github.com>
2026-09-13 00:19:46 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ import SocialIcons from "@/components/SocialIcons";
 import { services, policy } from "@/data/site";
 import GalleryCarousel from "@/components/GalleryCarousel";
 
-const basePath = "/yaegashi-portfolio";
-
 const sideNav = [
   { label: "Works", href: "/works" },
   { label: "Other", href: "/other" },
@@ -28,6 +26,7 @@ const isContain = (src: string) =>
     "/images/franny4.png",
     "/images/vivotree0.png",
     "/images/vivotree1.png",
+    "/images/screenshot01.png",
   ].includes(src);
 
 export default function Page() {

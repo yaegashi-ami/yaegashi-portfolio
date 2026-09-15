eeeac6b27f90c4b9c8caee4ce26c6403d1c4e18d
arcward-con <68462635+yaegashi-ami@users.noreply.github.com>
2026-09-15 17:55:12 +0900

    newsに追加

--- src/app/_components/NewsSection.tsx
+++ src/app/_components/NewsSection.tsx
@@ -15,8 +15,27 @@ export default function NewsSection() {
         </span>
         <span>News</span>
       </h2>
-      <ul className="flex w-full flex-col gap-3 text-sm leading-6">
-        <li className="flex items-baseline justify-between gap-4 border-[0.5px] border-main bg-white px-5 py-2.5 shadow-[3px_3px_0] shadow-main/100 md:px-6">
+      <ul className="flex w-full flex-col gap-2 text-sm leading-6">
+        <Link
+            href="/other/wordpress-ux"
+            className="group flex items-baseline justify-between rounded-lg gap-4 border-[0.5px] border-main bg-white px-5 py-2.5 shadow-main/100 focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-main md:px-6"
+          >
+            <span className="text-main">
+              <span className="underline-offset-4 group-hover:underline">
+                Other - WordPress管理画面のUI/UX改善 追加
+              </span>
+              <span
+                aria-hidden="true"
+                className="ms-fill ml-1 align-middle text-[18px]"
+              >
+                north_east
+              </span>
+            </span>
+            <span className="shrink-0 text-right tabular-nums text-muted">
+              2026/9/15
+            </span>
+          </Link>
+        <li className="flex items-baseline justify-between rounded-lg gap-4 border-[0.5px] border-main bg-white px-5 py-2.5 shadow-main/100 md:px-6">
           <span>ポートフォリオリニューアル</span>
           <span className="shrink-0 text-right tabular-nums text-muted">
             2026/9/12
@@ -25,7 +44,7 @@ export default function NewsSection() {
         <li>
           <Link
             href="/other#now-playing-music"
-            className="group flex items-baseline justify-between gap-4 border-[0.5px] border-main bg-white px-5 py-2.5 shadow-[3px_3px_0] shadow-main/100 focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-main md:px-6"
+            className="group flex items-baseline justify-between rounded-lg gap-4 border-[0.5px] border-main bg-white px-5 py-2.5 shadow-main/100 focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-main md:px-6"
           >
             <span className="text-main">
               <span className="underline-offset-4 group-hover:underline">
